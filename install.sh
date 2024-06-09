795629278a7ff9588f30048fc330ef46ba99b7e9
h3x0c4t <118213553+h3x0c4t@users.noreply.github.com>
2024-06-10 00:09:49 +0300

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Check if the script is running as root
 if [ $(id -u) -ne 0 ]; then
@@ -12,6 +12,7 @@ if [ ! -f /etc/debian_version ]; then
     exit 1
 fi
 
+# Get the username of the user who invoked sudo
 USER=$(logname)
 
 # Update the package list
@@ -20,16 +21,16 @@ apt update
 
 # Install the required packages
 echo "[*] Installing the required packages"
-apt install -y git network-manager dkms build-essential linux-headers-$(uname -r) fontconfig \ 
-                    fonts-jetbrains-mono fonts-noto fonts-recommended xorg libx11-dev libxft-dev \
-                    libxinerama-dev libx11-xcb-dev libxcb-res0-dev dmenu alacritty zsh dex \
-                    zsh-autosuggestions zsh-syntax-highlighting papirus-icon-theme wget curl network-manager-gnome
+apt install -y git network-manager dkms build-essential linux-headers-$(uname -r) fontconfig \
+    fonts-jetbrains-mono fonts-noto fonts-recommended xorg libx11-dev libxft-dev \
+    libxinerama-dev libx11-xcb-dev libxcb-res0-dev dmenu alacritty zsh dex \
+    zsh-autosuggestions zsh-syntax-highlighting papirus-icon-theme wget curl
 
 # Font configuration
 echo "[*] Configuring the fonts"
-sudo -u $USER mkdir -p ~/.config/fontconfig
-sudo -u $USER wget -O ~/.config/fontconfig/fonts.conf https://raw.githubusercontent.com/h3x0c4t/install/master/files/fonts.conf
-sudo -u $USER fv-cache -f
+sudo -u $USER mkdir -p /home/$USER/.config/fontconfig
+sudo -u $USER wget -O /home/$USER/.config/fontconfig/fonts.conf https://raw.githubusercontent.com/h3x0c4t/install/master/files/fonts.conf
+fc-cache -f
 
 # Install dwm
 echo "[*] Installing dwm"
@@ -38,34 +39,36 @@ git clone https://github.com/h3x0c4t/dwm-patched /usr/local/src/dwm
 
 # Create the .xinitrc file
 echo "[*] Creating the .xinitrc file"
-sudo -u $USER wget -O ~/.xinitrc https://raw.githubusercontent.com/h3x0c4t/install/master/files/.xinitrc
-sudo -u $USER chmod +x ~/.xinitrc
+sudo -u $USER wget -O /home/$USER/.xinitrc https://raw.githubusercontent.com/h3x0c4t/install/master/files/.xinitrc
+sudo -u $USER chmod +x /home/$USER/.xinitrc
 
 # Configure alacritty
 echo "[*] Configuring alacritty"
-sudo -u $USER mkdir -p ~/.config/alacritty
-sudo -u $USER wget -O ~/.config/alacritty/alacritty.yml https://raw.githubusercontent.com/h3x0c4t/install/master/files/alacritty.yml
+sudo -u $USER mkdir -p /home/$USER/.config/alacritty
+sudo -u $USER wget -O /home/$USER/.config/alacritty/alacritty.yml https://raw.githubusercontent.com/h3x0c4t/install/master/files/alacritty.yml
 
 # Configure zsh
 echo "[*] Configuring zsh"
-sudo -u $USER wget -O ~/.zshrc https://raw.githubusercontent.com/h3x0c4t/install/master/files/.zshrc
+sudo -u $USER wget -O /home/$USER/.zshrc https://raw.githubusercontent.com/h3x0c4t/install/master/files/.zshrc
 usermod --shell /bin/zsh $USER
 
 # Configure GTK
 echo "[*] Configuring GTK"
-sudo -u $USER mkdir -p ~/.local/share/themes
-sudo -u $USER git clone -b darker-standard-buttons https://github.com/EliverLara/Nordic .local/share/themes/Nordic-darker-standard-buttons
-sudo -u $USER mkdir -p ~/.config/gtk-3.0
-sudo -u $USER wget -O ~/.config/gtk-3.0/settings.ini https://raw.githubusercontent.com/h3x0c4t/install/master/files/settings.ini
+sudo -u $USER mkdir -p /home/$USER/.local/share/themes
+sudo -u $USER git clone -b darker-standard-buttons https://github.com/EliverLara/Nordic /home/$USER/.local/share/themes/Nordic-darker-standard-buttons
+
+sudo -u $USER mkdir -p /home/$USER/.config/gtk-3.0
+sudo -u $USER wget -O /home/$USER/.config/gtk-3.0/settings.ini https://raw.githubusercontent.com/h3x0c4t/install/master/files/settings.ini
 
 # Install nm-applet
 echo "[*] Installing nm-applet"
-sudo -u $USER mkdir -p ~/.config/autostart
-sudo -u $USER wget -O ~/.config/autostart/nm-applet.desktop https://raw.githubusercontent.com/h3x0c4t/install/master/files/nm-applet.desktop
+apt install -y network-manager-gnome
+sudo -u $USER mkdir -p /home/$USER/.config/autostart
+sudo -u $USER wget -O /home/$USER/.config/autostart/nm-applet.desktop https://raw.githubusercontent.com/h3x0c4t/install/master/files/nm-applet.desktop
 
 # Install guest additions
 echo "[*] Please insert the guest additions CD"
-read -p "[*] Press enter to continue" tmp_var
+read -p "[*] Press enter to continue" dummy_var
 
 mkdir -p /mnt/cdrom
 mount /dev/cdrom /mnt/cdrom
@@ -84,4 +87,3 @@ systemctl start NetworkManager
 
 # Done
 echo "[*] Installation complete! Please reboot the system."
-
